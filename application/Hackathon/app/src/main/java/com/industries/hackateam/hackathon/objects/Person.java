5cfe6913ebcd23e28639faee32932612b51ee015
Vincent Offroy <offroy.vincent@gmail.com>
2017-10-05 16:07:56 +0200

    add of sdk and plugin

---
+++ application/Hackathon/app/src/main/java/com/industries/hackateam/hackathon/objects/Person.java
@@ -0,0 +1,11 @@
+package com.industries.hackateam.hackathon.objects;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by vincent on 05/10/2017.
+ */
+
+public class Person extends RealmObject {
+    
+}
